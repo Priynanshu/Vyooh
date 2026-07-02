8c4873a64cd036313507c2b2d7f98ac980713c7c
Priyanshu Kumar <priyanshu3960@gmail.com>
2026-07-02 20:38:35 +0530

    Update CORS configuration to expose 'set-cookie' header

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,7 +27,7 @@ app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true ,
-    allowedHeaders: ['Content-Type', 'Authorization']
+    exposedHeaders: ['set-cookie']
 }))
 
 app.use(express.json())
